b0ff5a1da60f4f932a137f55f7a91112c03b4def
G <twenty14g@gmail.com>
2015-01-24 20:57:35 -0700

    Added average crime rate.

--- lowest_crime_rate.py
+++ lowest_crime_rate.py
@@ -1,10 +1,20 @@
 from operator import itemgetter
 
+total = 0
 crime_rates = list()
 with open('crime_rates.csv', 'r') as f:
     for line in f:
         city, rate = line.rstrip().split(',')
         crime_rates.append((city, int(rate)))
+        total += int(rate)
 
-print(max(crime_rates, key=itemgetter(1)))
-print(min(crime_rates, key=itemgetter(1)))
+hi_city, hi_rate = max(crime_rates, key=itemgetter(1))
+lo_city, lo_rate = min(crime_rates, key=itemgetter(1))
+avg_rate = total / len(crime_rates)
+
+print('''\
+Highest: {} - {} / 100,000 people
+Lowest:  {} - {} / 100,000 people
+
+Average crime rate of {} given cities is {:.2f} per 100,000
+'''.format(hi_city, hi_rate, lo_city, lo_rate, len(crime_rates), avg_rate))
