ac2d5c602c59881decbb2edf106e58c97353a9f6
G <twenty14g@gmail.com>
2015-01-24 22:22:35 -0700

    Created dataquest_scraper to grab the csv file from DataQuest.io result window frame.

---
+++ dataquest_scraper.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+from re import findall
+
+
+def scraper(filename):
+    with open(filename, 'r') as f:
+        soup = BeautifulSoup(f)
+    return findall(r"'(.*?)'", soup.text)
+
+
+def make_csv(filename, lst):
+    with open(filename, 'w') as f:
+        [f.write(a + '\n') for a in lst]
+
+make_csv('la_weather.csv', scraper('test.html'))
+
+
+# # enter below to grab it as a list
+# weather_data = []
+#
+# with open('la_weather.csv', 'r') as f:
+#     for line in f:
+#         weather_data.append(line.rstrip())
+# print(weather_data)
